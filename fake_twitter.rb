9796fc455adf543a5b4f8735cc7e03b9c5aef574
Zach Feldman <zachfeldman@gmail.com>
2013-07-18 19:11:34 -0400

    Added flashes, sessions, and links.

--- fake_twitter.rb
+++ fake_twitter.rb
@@ -2,10 +2,28 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+require 'bundler/setup'
+
 require 'sinatra/activerecord'
 configure(:development){ set :database, "sqlite3:///fake_twitter.sqlite3" }
 
 require './models'
+
+require 'sinatra/base'
+require 'rack-flash'
+
+enable :sessions 
+use Rack::Flash, :sweep => true
+
+set :sessions => true
+
+
+helpers do
+  def current_user
+    session[:user_id].nil? ? nil : User.find(session[:user_id])
+  end
+end
+
 
 get '/' do
   @users = User.all
@@ -20,15 +38,24 @@ post '/sign_in' do
   @user = User.where(:username => params[:username]).first
   if @user
     if @user.password == params[:password]
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back to Fake Twitter, #{@user.full_name}."
       redirect '/users/' + @user.id.to_s
     else
-      params[:alert] = "Your password was wrong."
+      flash[:notice] = "Your password was wrong."
       redirect '/'
     end
   else
+    flash[:notice] = "User not found. Please sign up for Fake Twitter."
     redirect '/users/sign_up'
   end
 end
+
+get '/sign_out' do
+  session[:user_id] = nil
+  flash[:notice] = "You have been signed out."
+  redirect '/'
+end 
 
 get '/users/sign_up' do
   haml :sign_up
